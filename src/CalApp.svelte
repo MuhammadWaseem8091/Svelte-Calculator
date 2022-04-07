aae1fbbe523f2d43c0ebfa496e132d535632cae6
MuhammadWaseem8091 <waseem.matto380@gmail.com>
2022-04-07 10:46:45 +0500

    My first git rem

---
+++ src/CalApp.svelte
@@ -0,0 +1,15 @@
+<script>
+	import Keypad from './calculator.svelte';
+
+	let pin;
+	$: view = pin ? pin : '0';
+
+    
+	function handleSubmit() {
+        alert(`submitted ${pin}`);
+	}
+</script>
+
+<h1 style="color: {pin ? '#333' : '#ccc'}">{view}</h1>
+
+<Keypad bind:value={pin} on:submit={handleSubmit}/>
---
+++ src/Prctice.svelte
@@ -0,0 +1,12 @@
+<script>
+	
+</script>
+
+<main>
+	<h1>machine lrarojnnvv !</h1>
+	<h1>Hel[sdfnskjdfksdfsfdkjnaame is !</h1>
+</main>
+
+<style>
+	
+</style>
